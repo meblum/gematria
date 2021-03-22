3b0a4bef81c9d32250cb5cbe405de3a9a2ea5cbf
Meir Blumenfeld <emmbee7806@gmail.com>
2021-03-21 21:34:05 -0400

    runeValues

---
+++ gematria.go
@@ -0,0 +1,84 @@
+package gematria
+
+var runeValues = map[rune]int{
+	rune(1488): 1,
+	rune(1489): 2,
+	rune(1490): 3,
+	rune(1491): 4,
+	rune(1492): 5,
+	rune(1493): 6,
+	rune(1494): 7,
+	rune(1495): 8,
+	rune(1496): 9,
+	rune(1497): 10,
+	rune(1499): 20,
+	rune(1500): 30,
+	rune(1502): 40,
+	rune(1504): 50,
+	rune(1505): 60,
+	rune(1506): 70,
+	rune(1508): 80,
+	rune(1510): 90,
+	rune(1511): 100,
+	rune(1512): 200,
+	rune(1513): 300,
+	rune(1514): 400,
+
+	rune(1478): 50, // nun hufich
+
+	rune(1519): 30, //triple yid
+	rune(1520): 12, //double vuv
+	rune(1521): 16, //vuv yid
+	rune(1522): 20, //yid yid
+
+	rune(1498): 20, //enda
+	rune(1501): 40, //enda
+	rune(1503): 50, //enda
+	rune(1507): 80, //enda
+	rune(1509): 90, //enda
+
+	rune(64285): 10,
+	rune(64287): 20, //double yid
+	rune(64288): 70,
+	rune(64289): 1,
+	rune(64290): 4,
+	rune(64291): 5,
+	rune(64292): 20,
+	rune(64293): 30,
+	rune(64294): 40, //enda
+	rune(64295): 200,
+	rune(64296): 400,
+	rune(64298): 300,
+	rune(64299): 300,
+	rune(64300): 300,
+	rune(64301): 300,
+	rune(64302): 1,
+	rune(64303): 1,
+	rune(64304): 1,
+	rune(64305): 2,
+	rune(64306): 3,
+	rune(64307): 4,
+	rune(64308): 5,
+	rune(64309): 6,
+	rune(64310): 7,
+	rune(64312): 9,
+	rune(64313): 10,
+	rune(64314): 20, // ENDA
+	rune(64315): 20,
+	rune(64316): 30,
+	rune(64318): 40,
+	rune(64320): 50,
+	rune(64321): 60,
+	rune(64323): 80, //enda
+	rune(64324): 80,
+	rune(64326): 90,
+	rune(64327): 100,
+	rune(64328): 200,
+	rune(64329): 300,
+	rune(64330): 400,
+	rune(64331): 6,
+	rune(64332): 2,
+	rune(64333): 20,
+	rune(64334): 80,
+	rune(64335): 31, //alef lamed
+}
